0a2571fe84984234305f7bd8e729d46443b1b9c7
Anjie Zheng <junaeo@gmail.com>
2016-08-11 16:59:46 +0800

    Update main.cpp

--- B-matching/main.cpp
+++ B-matching/main.cpp
@@ -1,8 +1,8 @@
 //
 //  main.cpp
-//  graph
+//  GG
 //
-//  Created by Dreamcastle on 2016/8/1.
+//  Created by Dreamcastle on 2016/8/11.
 //  Copyright © 2016年 Dreamcastle. All rights reserved.
 //
 
@@ -10,8 +10,16 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cstring>
+#include <cstdlib>
 
 using namespace std;
+
+string a1;
+string a2;
+string a3;
+string a4;
+string a5;
 
 struct Vertex {
     long vertexID;
@@ -29,46 +37,54 @@ vector<struct Edge> combination;
 
 long sumOfWeight = 0;
 
-void check(vector<struct Edge> comb,  string argv1,string argv2,string argv3,string argv4, string argv5) {
-    vector<struct Vertex> check_v;
-    check_v = v;
+void countWeight(vector<struct Edge> comb) {
     long tmp_max = 0;
     for(int i = 0; i<comb.size(); i++){
-        //cout<< "src: "<< comb[i].srcID<< ", dest: "<< comb[i].destinID<< ", atr: "<< comb[i].attr<< endl;
+        tmp_max += comb[i].attr;
+    }
+    if(tmp_max > sumOfWeight){
+        sumOfWeight = tmp_max;
+        
+        /*
+        string boutputtext = string("./")+a1.c_str()+string("-")+a2.c_str()+string("-")+a3.c_str()+string("-")+a4.c_str()+string("-")+a5.c_str()+string("-boutput.txt");
+        
+        ofstream f;
+        f.open(boutputtext.c_str(),ios_base::out);
+        
+
+        for(int i = 0; i<comb.size(); i++){
+            f<< "Edge("<< comb[i].srcID<< ","<< comb[i].destinID<< ","<< comb[i].attr<<")"<<endl;
+        }
+        */
+    }
+}
+
+bool checkEdge(vector<struct Edge> comb){
+    vector<struct Vertex> check_v;
+    check_v = v;
+    for(int i = 0; i<comb.size(); i++){
         
         check_v[comb[i].srcID].capacity--;
         check_v[comb[i].destinID].capacity--;
         
         if(check_v[comb[i].srcID].capacity == -1 || check_v[comb[i].destinID].capacity == -1){
-            //cout<< "----------"<<endl;
-            return;
-        }else{
-            tmp_max += comb[i].attr;
+            return false;
         }
     }
-    if(tmp_max > sumOfWeight){
-        sumOfWeight = tmp_max;
-
-        ofstream file;
-        file.open("./"+argv1+"-"+argv2+"-"+argv3+"-"+argv4+"-"+argv5+"-boutput.txt",ios_base::out);
-
-
-        for(int i = 0; i<comb.size(); i++){
-            file<< "Edge("<< comb[i].srcID<< ","<< comb[i].destinID<< ","<< comb[i].attr<<")"<<endl;
-        }
-    }
-    //cout<< "-------"<<tmp_max<<"-------"<<endl;
+    return true;
 }
 
-
-void go(int offset, int k, string argv1,string argv2,string argv3,string argv4, string argv5) {
+void go(int offset, int k) {
+    if(checkEdge(combination) == false){
+        return;
+    }
     if (k == 0) {
-        check(combination, argv1, argv2, argv3, argv4, argv5);
+        countWeight(combination);
         return;
     }
     for (int i = offset; i <= e.size() - k; ++i) {
         combination.push_back(e[i]);
-        go(i+1, k-1, argv1, argv2, argv3, argv4, argv5);
+        go(i+1, k-1);
         combination.pop_back();
     }
 }
@@ -80,81 +96,96 @@ int main(int argc,char *argv[])
     string argv3(argv[3]);
     string argv4(argv[4]);
     string argv5(argv[5]);
-
-
-        string se = "./"+argv1+"-"+argv2+"-"+argv3+"-"+argv4+"-"+argv5+"-edge.txt";
-        ofstream file;
-        file.open(se,ios_base::out);
-        file.close();
-        file.open("./"+argv1+"-"+argv2+"-"+argv3+"-"+argv4+"-"+argv5+"-vertex.txt",ios_base::out);
-        file.close();
+    
+    a1 = argv1;
+    a2 = argv2;
+    a3 = argv3;
+    a4 = argv4;
+    a5 = argv5;
+    
+    string edgetext = string("./")+argv1.c_str()+string("-")+argv2.c_str()+string("-")+argv3.c_str()+string("-")+argv4.c_str()+string("-")+argv5.c_str()+string("-edge.txt");
+    string vertextext = string("./")+argv1.c_str()+string("-")+argv2.c_str()+string("-")+argv3.c_str()+string("-")+argv4.c_str()+string("-")+argv5.c_str()+string("-vertex.txt");
+    string bresulttext = string("./")+argv1.c_str()+string("-")+argv2.c_str()+string("-")+argv3.c_str()+string("-")+argv4.c_str()+string("-")+argv5.c_str()+string("-bresult.txt");
+    string poutput = string("./")+argv1.c_str()+string("-")+argv2.c_str()+string("-")+argv3.c_str()+string("-")+argv4.c_str()+string("-")+argv5.c_str()+string("-poutput.txt");
+    string presult = string("./")+argv1.c_str()+string("-")+argv2.c_str()+string("-")+argv3.c_str()+string("-")+argv4.c_str()+string("-")+argv5.c_str()+string("-presult.txt");
+    
+    
+    ofstream file;
+    file.open(edgetext.c_str(),ios_base::out);
+    file.close();
+    file.open(vertextext.c_str(),ios_base::out);
+    file.close();
+    file.open(poutput.c_str(),ios_base::out);
+    file.close();
+    file.open(presult.c_str(),ios_base::out);
+    file.close();
+    
+    int vertexNum = atoi(argv[1]);
+    int maxCapacity = atoi(argv[2]);
+    
+    unsigned seed;
+    seed = (unsigned)time(NULL);
+    srand(seed);
+    
+    struct Vertex tmp;
+    tmp.vertexID = 0;
+    tmp.capacity = -1;
+    v.push_back(tmp);
+    
+    file.open(vertextext.c_str(),ios_base::app);
+    
+    for(int i = 1; i <= vertexNum ; i++){
         
-        int vertexNum = atoi(argv[1]);
-        int maxCapacity = atoi(argv[2]);
+        tmp.vertexID = i;
+        tmp.capacity = (rand() % maxCapacity) + 1;
         
-        unsigned seed;
-        seed = (unsigned)time(NULL);
-        srand(seed);
-        
-        struct Vertex tmp;
-        tmp.vertexID = 0;
-        tmp.capacity = -1;
         v.push_back(tmp);
         
-        file.open("./"+argv1+"-"+argv2+"-"+argv3+"-"+argv4+"-"+argv5+"-vertex.txt",ios_base::app);
+        file << tmp.vertexID << " " << tmp.capacity << endl;
         
-        for(int i = 1; i <= vertexNum ; i++){
-
-            tmp.vertexID = i;
-            tmp.capacity = (rand() % maxCapacity) + 1;
-            
-            v.push_back(tmp);
-            
-            file << tmp.vertexID << " " << tmp.capacity << endl;
-            
-            
-            //cout<< i <<" "<< v[i].capacity <<endl;
-        }
         
-        file.close();
-        
-        int maxWeight = atoi(argv[3]);
-        int alpha = atoi(argv[4]);
-        
-        file.open("./"+argv1+"-"+argv2+"-"+argv3+"-"+argv4+"-"+argv5+"-edge.txt",ios_base::app);
-        
-        for(int i = 1; i <= vertexNum; i++){
-            for(int j = i; j <= vertexNum; j++){
-                if(i == j) continue;
-                else {
-                    if(rand() % alpha == 1){
-                        
-                        struct Edge tmp;
-                        tmp.srcID = i;
-                        tmp.destinID = j;
-                        tmp.attr = (rand() % maxWeight) + 1;
-                        
-                        e.push_back(tmp);
-                        
-                        file << tmp.srcID << " " << tmp.destinID  << " " << tmp.attr << endl;
-                        
-                        
-                    }else{
-                        continue;
-                    }
+        //cout<< i <<" "<< v[i].capacity <<endl;
+    }
+    
+    file.close();
+    
+    int maxWeight = atoi(argv[3]);
+    int alpha = atoi(argv[4]);
+    
+    file.open(edgetext.c_str(),ios_base::app);
+    
+    for(int i = 1; i <= vertexNum; i++){
+        for(int j = i; j <= vertexNum; j++){
+            if(i == j) continue;
+            else {
+                if(rand() % alpha == 1){
+                    
+                    struct Edge tmp;
+                    tmp.srcID = i;
+                    tmp.destinID = j;
+                    tmp.attr = (rand() % maxWeight) + 1;
+                    
+                    e.push_back(tmp);
+                    
+                    file << tmp.srcID << " " << tmp.destinID  << " " << tmp.attr << endl;
+                    
+                    
+                }else{
+                    continue;
                 }
             }
         }
-        
-        file.close();
-        
-        for(int i = 1; i < e.size(); i++){
-            go(1, i, argv1, argv2, argv3, argv4, argv5);
-        }
-        
-        file.open("./"+argv1+"-"+argv2+"-"+argv3+"-"+argv4+"-"+argv5+"-bresult.txt",ios::out);
-        file << sumOfWeight << endl;
-        file.close();
+    }
+    
+    file.close();
+    
+    for(int i = 1; i <= e.size(); i++){
+        go(0, i);
+    }
+    
+    file.open(bresulttext.c_str(),ios::out);
+    file << sumOfWeight << endl;
+    file.close();
     
     return 0;
 }
